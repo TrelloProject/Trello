d1c11dce311181dc73e52d1fd724af776aab7a96
sillysillyman <sillysillyman.cs@gmail.com>
2024-07-10 18:19:12 +0900

    [Feat]: Card 엔티티 구현

--- src/main/java/com/sparta/trello/domain/card/entity/Card.java
+++ src/main/java/com/sparta/trello/domain/card/entity/Card.java
@@ -1,15 +1,44 @@
 package com.sparta.trello.domain.card.entity;
 
+import com.sparta.trello.common.TimeStampEntity;
+import com.sparta.trello.domain.column.Deck;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import java.time.LocalDate;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 @Entity
 @Table(name = "cards")
-public class Card {
+public class Card extends TimeStampEntity {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "deck_id", nullable = false)
+    private Deck deck;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    private Long nextId;
+
+    private String title;
+
+    private String description;
+
+    private LocalDate startDate;
+
+    private LocalDate dueDate;
 }
