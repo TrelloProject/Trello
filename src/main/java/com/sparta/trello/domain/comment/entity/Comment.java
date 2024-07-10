7f64e96f17581d4d23704589e1e525f7fa78c399
sillysillyman <sillysillyman.cs@gmail.com>
2024-07-10 18:17:20 +0900

    [Feat]: Comment 엔티티 구현

---
+++ src/main/java/com/sparta/trello/domain/comment/entity/Comment.java
@@ -0,0 +1,35 @@
+package com.sparta.trello.domain.comment.entity;
+
+import com.sparta.trello.common.TimeStampEntity;
+import com.sparta.trello.domain.card.entity.Card;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@Entity
+@Table(name = "comments")
+public class Comment extends TimeStampEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "card_id", nullable = false)
+    private Card card;
+
+    private String content;
+}
