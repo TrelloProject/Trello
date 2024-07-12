e8f49bd456ea84b4c63354dfd488849bf0735f4c
sillysillyman <sillysillyman.cs@gmail.com>
2024-07-12 20:12:46 +0900

    [Refactor]: Deck 엔티티 수정
    - headCardId 필드에 @Setter 추가
    - 구글 스타일 가이드 적용

--- src/main/java/com/sparta/trello/domain/deck/entity/Deck.java
+++ src/main/java/com/sparta/trello/domain/deck/entity/Deck.java
@@ -2,10 +2,19 @@ package com.sparta.trello.domain.deck.entity;
 
 import com.sparta.trello.common.TimeStampEntity;
 import com.sparta.trello.domain.board.entity.Board;
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Getter
@@ -23,6 +32,7 @@ public class Deck extends TimeStampEntity {
     @Column
     private Long nextId;
 
+    @Setter
     @Column
     private Long headCardId;
 
@@ -31,17 +41,17 @@ public class Deck extends TimeStampEntity {
     private Board board;
 
     @Builder
-    public Deck(String title, Long nextId, Board board){
+    public Deck(String title, Long nextId, Board board) {
         this.title = title;
         this.nextId = nextId;
         this.board = board;
     }
 
-    public void updateTitle(String title){
+    public void updateTitle(String title) {
         this.title = title;
     }
 
-    public void updateNextId(Long nextId){
+    public void updateNextId(Long nextId) {
         this.nextId = nextId;
     }
 }
