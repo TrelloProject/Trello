09a1f06ba8d52318e34b237f3d65dd5739bad36b
sillysillyman <sillysillyman.cs@gmail.com>
2024-07-12 19:46:42 +0900

    [Feat]: CardRepository 메서드 추가
    - 카드 이동 및 검색을 위한 메서드 추가
      - findCardByNextId(Long cardId)
      - findTopByDeckIdOrderByIdDesc(Long deckId)
      - findAllByDeckId(Long deckId)

--- src/main/java/com/sparta/trello/domain/card/repository/CardRepository.java
+++ src/main/java/com/sparta/trello/domain/card/repository/CardRepository.java
@@ -1,8 +1,15 @@
 package com.sparta.trello.domain.card.repository;
 
 import com.sparta.trello.domain.card.entity.Card;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CardRepository extends JpaRepository<Card, Long> {
 
+    Card findCardByNextId(Long cardId);
+
+    Optional<Card> findTopByDeckIdOrderByIdDesc(Long deckId);
+
+    List<Card> findAllByDeckId(Long deckId);
 }
