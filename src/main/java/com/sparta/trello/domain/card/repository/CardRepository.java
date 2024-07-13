e0789dbfe3513cbdb14f35ea5613f18c94437743
sillysillyman <sillysillyman.cs@gmail.com>
2024-07-13 20:10:59 +0900

    [Feat]: Fetch Join 메서드 구현
    - Card 엔티티와 연관관계에 있는 엔티티에 대해 Fetch Join 메서드 구현

--- src/main/java/com/sparta/trello/domain/card/repository/CardRepository.java
+++ src/main/java/com/sparta/trello/domain/card/repository/CardRepository.java
@@ -4,6 +4,8 @@ import com.sparta.trello.domain.card.entity.Card;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface CardRepository extends JpaRepository<Card, Long> {
 
@@ -12,4 +14,13 @@ public interface CardRepository extends JpaRepository<Card, Long> {
     Optional<Card> findTopByDeckIdOrderByIdDesc(Long deckId);
 
     List<Card> findAllByDeckId(Long deckId);
+
+    @Query("SELECT c FROM Card c JOIN FETCH c.deck WHERE c.id = :cardId")
+    Optional<Card> findByIdWithDeck(@Param("cardId") Long cardId);
+
+    @Query("SELECT c FROM Card c JOIN FETCH c.user WHERE c.id = :cardId")
+    Optional<Card> findByIdWithUser(@Param("cardId") Long cardId);
+
+    @Query("SELECT c FROM Card c JOIN FETCH c.deck JOIN FETCH c.user WHERE c.id = :cardId")
+    Optional<Card> findByIdWithDeckAndUser(@Param("cardId") Long cardId);
 }
