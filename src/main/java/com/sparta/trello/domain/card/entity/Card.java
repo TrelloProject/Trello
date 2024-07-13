1583d647588eb202caf6a6292d268a80016c85a8
sillysillyman <sillysillyman.cs@gmail.com>
2024-07-13 20:09:00 +0900

    [Feat]: Card 엔티티 인덱스 생성
    - deck_id 인덱스 생성
    - next_id 인덱스 생성
    - title, description, start_date, due_date 복합 인덱스 생성

--- src/main/java/com/sparta/trello/domain/card/entity/Card.java
+++ src/main/java/com/sparta/trello/domain/card/entity/Card.java
@@ -8,6 +8,7 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Index;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
@@ -22,7 +23,11 @@ import org.hibernate.annotations.OnDeleteAction;
 @Getter
 @NoArgsConstructor
 @Entity
-@Table(name = "card")
+@Table(name = "card", indexes = {
+    @Index(name = "idx_card_deck_id", columnList = "deck_id"),
+    @Index(name = "idx_card_next_id", columnList = "next_id"),
+    @Index(name = "idx_card_fields", columnList = "title, description, start_date, due_date")
+})
 public class Card extends TimeStampEntity {
 
     @Id
