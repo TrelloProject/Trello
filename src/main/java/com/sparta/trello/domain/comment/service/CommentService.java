3a0541404729ea8fcf7329a67d774c3ff7b10e6f
sillysillyman <sillysillyman.cs@gmail.com>
2024-07-15 04:14:23 +0900

    [Feat]: 보드 멤버 검증 추가
    - BoardMemberAdapter를 사용하여 사용자가 보드 멤버인지 확인하는 검증 로직 추가

--- src/main/java/com/sparta/trello/domain/comment/service/CommentService.java
+++ src/main/java/com/sparta/trello/domain/comment/service/CommentService.java
@@ -1,6 +1,10 @@
 package com.sparta.trello.domain.comment.service;
 
+import com.sparta.trello.domain.board.entity.Board;
+import com.sparta.trello.domain.boardMember.entity.BoardMember;
+import com.sparta.trello.domain.boardMember.repository.BoardMemberAdapter;
 import com.sparta.trello.domain.card.adapter.CardAdapter;
+import com.sparta.trello.domain.card.entity.Card;
 import com.sparta.trello.domain.comment.dto.CreateCommentRequestDto;
 import com.sparta.trello.domain.comment.dto.UpdateCommentRequestDto;
 import com.sparta.trello.domain.comment.entity.Comment;
@@ -18,10 +22,17 @@ public class CommentService {
 
     private final CardAdapter cardAdapter;
     private final CommentAdapter commentAdapter;
+    private final BoardMemberAdapter boardMemberAdapter;
 
     @Transactional
     public void createComment(CreateCommentRequestDto createCommentRequestDto,
         User user) {
+
+        // 보드 멤버 검증
+        Card card = cardAdapter.findById(createCommentRequestDto.getCardId());
+        Board board = card.getDeck().getBoard();
+        BoardMember boardMember = boardMemberAdapter.findByBoardAndUser(board, user);
+        boardMemberAdapter.validateBoardMember(boardMember);
 
         Comment comment = Comment.builder()
             .user(user)
@@ -37,6 +48,12 @@ public class CommentService {
 
         Comment comment = commentAdapter.findById(commentId);
         commentAdapter.validateCommentOwnership(comment, user);
+
+        // 보드 멤버 검증
+        Board board = comment.getCard().getDeck().getBoard();
+        BoardMember boardMember = boardMemberAdapter.findByBoardAndUser(board, user);
+        boardMemberAdapter.validateBoardMember(boardMember);
+
         comment.setContent(updateCommentRequestDto.getContent());
         commentAdapter.save(comment);
     }
@@ -45,6 +62,12 @@ public class CommentService {
     public void deleteComment(Long commentId, User user) {
         Comment comment = commentAdapter.findById(commentId);
         commentAdapter.validateCommentOwnership(comment, user);
+
+        // 보드 멤버 검증
+        Board board = comment.getCard().getDeck().getBoard();
+        BoardMember boardMember = boardMemberAdapter.findByBoardAndUser(board, user);
+        boardMemberAdapter.validateBoardMember(boardMember);
+        
         commentAdapter.delete(comment);
     }
 }
