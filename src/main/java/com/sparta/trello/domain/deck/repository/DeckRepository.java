86575e92bb34673aa03d9198082396281cb1966d
sillysillyman <sillysillyman.cs@gmail.com>
2024-07-16 16:59:45 +0900

    [Feat]: findAllByBoardId 추가

--- src/main/java/com/sparta/trello/domain/deck/repository/DeckRepository.java
+++ src/main/java/com/sparta/trello/domain/deck/repository/DeckRepository.java
@@ -2,14 +2,16 @@ package com.sparta.trello.domain.deck.repository;
 
 import com.sparta.trello.domain.board.entity.Board;
 import com.sparta.trello.domain.deck.entity.Deck;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-import java.util.Optional;
-
 @Repository
 public interface DeckRepository extends JpaRepository<Deck, Long>, DeckQueryRepository {
+
+    List<Deck> findAllByBoardId(Long boardId);
+
     List<Deck> findByBoard(Board board);
 
     Optional<Deck> findByNextIdAndBoardId(Long nextId, Long boardId);
